7f3a302abbd43bba2ecbe42ba810d2bf1cedd6c7
Niall Liu <easlee@outlook.com>
2024-04-26 23:07:46 +0800

    first version dist

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# UltraDesk-Repository
+# 启动
+## python依赖包
+colorlog
+flask
+## 本机
+python run.py
+
+# volume结构
+```
+|- volume
+  |- applications
+    |- {os}
+      |- {Application}
+        |- {Application}_{version}.7z
+        |- meta.json
+  |- source_{os}.json
+```
+
+{os}的值集合为[windows]
+meta.json的内容，当前版本为空文件。
+
+如果volume/source_{os}.json不存在，服务启动后会自动生成。
+如果volume/applications目录下更新了Application，需要手动删除volume/source_{os}.json文件并重启服务重新生成。
+
+例如添加一个Chrome的120.0.6099.130的版本，volume的结构如下
+```
+|- volume
+  |- applications
+    |- windows
+      |- Chrome
+        |- Chrome_120.0.6099.130.7z
+        |- meta.json
+```
+启动服务后，会自动生成volume/source_windows.json
---
+++ run.py
@@ -0,0 +1,106 @@
+import os
+import json
+import datetime
+import hashlib
+import logging
+import colorlog
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+volume_root = "./volume"
+
+
+def buildLogger(_logger):
+    log_colors_config = {
+        "DEBUG": "white",
+        "INFO": "green",
+        "WARING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "bold_red",
+    }
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        colorlog.ColoredFormatter(
+            fmt="%(log_color)s%(asctime)s [%(levelname)s] - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            log_colors=log_colors_config,
+        )
+    )
+    _logger.addHandler(handler)
+    _logger.setLevel(logging.DEBUG)
+    handler.close()
+
+
+def encrypt(_file):
+    with open(_file, "rb") as f:
+        hash = hashlib.new("sha256")
+        for chunk in iter(lambda: f.read(2**20), b""):
+            hash.update(chunk)
+        return hash.hexdigest()
+
+
+def generateApplication(_volume_root, _os):
+    applications_path = os.path.join(_volume_root, "applications").replace("\\", "/")
+    os_path = os.path.join(applications_path, _os).replace("\\", "/")
+    source_path = os.path.join(_volume_root, "source_{}.json".format(_os)).replace(
+        "\\", "/"
+    )
+    if not os.path.exists(os_path):
+        os.makedirs(os_path)
+    if os.path.exists(source_path):
+        log.info("found {}".format(source_path))
+        return
+
+    log.info("generate {}".format(source_path))
+
+    manifest = {}
+    manifest["version"] = datetime.datetime.utcnow().strftime("%Y%m%d%H%M")
+    manifest["applications"] = []
+    for app_name in os.listdir(os_path):
+        app_path = os.path.join(os_path, app_name).replace("\\", "/")
+        meta_path = os.path.join(app_path, "meta.json")
+        if not os.path.exists(meta_path):
+            continue
+
+        application = {}
+        application["name"] = app_name
+        application["packages"] = []
+        for pkg_name in os.listdir(app_path):
+            if not pkg_name.endswith(".7z"):
+                continue
+            log.debug("add " + pkg_name + "...")
+            package = {}
+            package["version"] = pkg_name.split("_")[-1].replace(".7z", "")
+            package["path"] = os.path.join(app_path, pkg_name).replace("\\", "/")
+            package["sha256"] = encrypt(package["path"])
+            application["packages"].append(package)
+
+        manifest["applications"].append(application)
+
+    with open(source_path, "w", encoding="utf-8") as f:
+        json.dump(manifest, f, ensure_ascii=False)
+
+
+@app.route("/<filename>")
+def download(filename):
+    log.info("download {}/{}".format(volume_root, filename))
+    return send_from_directory(
+        volume_root, filename, mimetype="application/octet-stream"
+    )
+
+
+"""
+main
+"""
+if __name__ == "__main__":
+    log = logging.getLogger("UltraDesk.Repository")
+    buildLogger(log)
+    log.info("UltraDesk Repository run ...")
+    log.info("---------------------------------------------------------")
+
+    if not os.path.exists(volume_root):
+        os.makedirs(volume_root)
+
+    generateApplication(volume_root, "windows")
+
+    app.run(host="0.0.0.0", port=80)
